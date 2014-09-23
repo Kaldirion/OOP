d2d06cb359d19b7ae51feb1ce364a9b2c3c89ecd
Kaldirion <kaldirion@gmail.com>
2014-09-23 23:20:43 +0300

    Homework 02
    Homework 2 for SoftUni

---
+++ Homework_02/01_Square_Roots/SqrRoot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Homework_02._01_Square_Roots
+{
+    class SqrRoot
+    {
+        static void Main()
+        {
+            try
+            {
+                Console.WriteLine("Please enter digit");
+                int Num = int.Parse(Console.ReadLine());
+                Console.WriteLine("Square root of " + Num + " is " + Math.Sqrt((double)Num));
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Must enter number!");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Must enter valid integer!");
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("Please enter number in the ragne of 32bit Integer!");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Homework_02/02_Enter_Numbers/EnterNumbers.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_02._02_Enter_Numbers
+{
+    class EnterNumbers
+    {
+        static void Main()
+        {
+            int start = 1;
+            int end = 100;
+            for (int i = 1; i <= 10; i++)
+                {
+                    start = ReadNumber(start, end);
+                }
+        }
+
+        static int ReadNumber(int start, int end)
+        {
+            int num = 0;
+            try
+            {
+                Console.Write("Enter number {0} < your number < {1}: ", start, end);
+                num = int.Parse(Console.ReadLine());
+                if (!(start < num && num < end))
+                {
+                    while (!(start < num && num < end))
+                    {
+                        Console.WriteLine("Your number is not in range {0} - {1} !", start, end);
+                        Console.Write("Enter number {0} < your number < {1}: ", start, end);
+                        num = int.Parse(Console.ReadLine());
+                    }
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid Number!");
+                throw;
+            }
+            return num;
+        }
+    }
+}
