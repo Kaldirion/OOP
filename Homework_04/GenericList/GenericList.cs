406e66748569cbe7ac8683aa20c5b179d6ebcd6a
Kaldirion <kaldirion@gmail.com>
2014-09-28 15:54:06 +0300

    Homework_04

---
+++ Homework_04/GenericList/GenericList.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    public class GenericList<T> where T:IComparable<T>
+    {
+        //Fields and Constants
+        const int DefaultCapacity = 16;
+
+        private T[] elements;
+        private int count = 0;
+
+        //Constructor
+        public GenericList(int capacity = DefaultCapacity)
+        {
+            elements = new T[capacity];
+        }
+
+        //Property
+        public int Count
+        {
+            get { return this.count; }
+        }
+
+        public int Capacity
+        {
+            get { return this.elements.Length; }
+        }
+
+        //Methods 
+        public void Add(T element)
+        {
+            if (count >= elements.Length)
+            {
+                throw new IndexOutOfRangeException(String.Format(
+                    "The list capacity of {0} was exceeded.", elements.Length));
+            }
+            this.elements[count] = element;
+            count++;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                {
+                    throw new IndexOutOfRangeException(String.Format(
+                        "Invalid index: {0}.", index));
+                }
+                T result = elements[index];
+                return result;
+            }
+        }
+
+        public bool Find(T element)
+        {
+            bool result = false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (Compare(this[i], element))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public  int FindIndex(T element)
+        {
+            if (this.Find(element))
+            {
+                int index = 0;
+                for (int i = 0; i < Count; i++)
+                {
+                    if (Compare(this[i], element))
+                    {
+                        index = i;
+                        break;
+                    }
+                }
+                return index;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+        public void InsertAt(T element, int index)
+        {
+            if (index < 0 || index > this.count)
+            {
+                throw new ArgumentOutOfRangeException(String.Format(
+                        "Invalid index: {0}.", index));
+            }
+            if (index >= this.Capacity)
+            {
+                this.Grow();
+            }
+
+            for (int i = this.count; i > index; i--)
+            {
+                this.elements[i] = this.elements[i - 1];
+            }
+            this.count++;
+            this.elements[index] = element;
+            
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index > this.count)
+            {
+                throw new ArgumentOutOfRangeException(String.Format("Invalid index : {0}", index));
+            }
+
+            for (int i = index; i < this.count - 1; i++)
+            {
+                this.elements[i] = this.elements[i + 1];
+            }
+            this.count--;
+            this.elements[count] = default(T);
+        }
+
+        public void ClearAll()
+        {
+            for (int i = 0; i < this.count; i++)
+            {
+                this.elements[i] = default(T);
+            }
+        }
+
+        public T Min(){
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            else
+            {
+                T minVal = this.elements[0];
+                for (int i = 0; i < this.count; i++)
+                {
+                    if (this.elements[i].CompareTo(minVal) < 0)
+                    {
+                        minVal = this.elements[i];
+                    }
+                }
+                return minVal;
+            }
+        }
+
+        public T Max()
+        {
+            if (this.count == 0)
+            {
+                throw new InvalidOperationException("The list is empty");
+            }
+            else
+            {
+                T maxVal = this.elements[0];
+                for (int i = 0; i < this.count; i++)
+                {
+                    if (this.elements[i].CompareTo(maxVal) > 0)
+                    {
+                        maxVal = this.elements[i];
+                    }
+                }
+                return maxVal;
+            }
+        }
+
+        private void Grow(){
+            T[] expandedArray = new T[this.Capacity * 2];
+            Array.Copy(this.elements, expandedArray, this.Capacity);
+            this.elements = expandedArray;
+        }
+
+        bool Compare(T element1, T element2)
+        {
+            return element1.Equals(element2);
+        }
+
+        public override string ToString()
+        {
+            string result = "";
+            Console.WriteLine("Number of elements: {0}", Count);
+            for (int i = 0; i < Count; i++)
+            {
+                T element = this[i];
+                result += "["+i+" -> "+element+"]\n";
+            }
+            return result;
+        }
+    }
+}
---
+++ Homework_04/GenericList/GenericListTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericList
+{
+    class GenericListTest
+    {
+        static void Main()
+        {
+            GenericList<int> intList = new GenericList<int>();
+            intList.Add(1);
+            intList.Add(2);
+            intList.Add(3);
+            Console.WriteLine(intList);
+            Console.WriteLine("Element at index 2 is : {0}",intList[2]);
+            Console.WriteLine("Dooes the list contain 1 : {0}",intList.Find(1));
+            Console.WriteLine("The index if value 3 is : {0}",intList.FindIndex(3));
+            intList.InsertAt(8, 2);
+            Console.WriteLine(intList);
+            intList.RemoveAt(2);
+            Console.WriteLine(intList);
+            //intList.ClearAll();
+            //Console.WriteLine(intList);
+            Console.WriteLine("Max Value : {0}",intList.Max());
+            Console.WriteLine("Min Value : {0}", intList.Min());
+            Console.ReadKey();
+        }
+    }
+}
